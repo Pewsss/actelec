e6d8b7a0ad68a76f76a1f55a9af205e4ce8c1f25
Pewsss <cristiannhestine30@gmail.com>
2025-10-28 15:41:08 +0800

    sub function

--- calculator.js
+++ calculator.js
@@ -2,7 +2,22 @@ function add (a, b){
     return a+b;
 }
 
+function sub(a, b){
+    return a-b;
+}
+
+
+
+
+
+
+
+
+
+
+
 let a = 4;
 let b = 3;
 
 console.log("add :" + add(a,b) );
+console.log("sub :" + sub(a,b) );
