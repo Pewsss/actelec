f0e4faebc44037c847ef1438d083921150125120
Pewsss <cristiannhestine30@gmail.com>
2025-10-28 15:56:32 +0800

    remove add commit

--- calculator.js
+++ calculator.js
@@ -1,7 +1,3 @@
-function add (a, b){ 
-    return a+b;
-}
-
 function sub(a, b){
     return a-b;
 }
@@ -26,7 +22,7 @@ function square(a,b){
 let a = 4;
 let b = 3;
 
-console.log("add :" + add(a,b) );
+
 console.log("sub :" + sub(a,b) );
 console.log("multiply :" + multiply(a,b) );
 console.log("divide :" + divide(a,b) );
